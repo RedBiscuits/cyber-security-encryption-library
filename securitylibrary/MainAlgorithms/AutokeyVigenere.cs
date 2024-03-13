aefe51aa5a858cfb1c17e6e0df7b0b718e1ea743
Kareem Hussen <KareemHussen500@gmail.com>
2024-03-13 16:32:26 +0200

    AutoKey & Repeat Done El Done

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -8,25 +8,103 @@ namespace SecurityLibrary
 {
     public class AutokeyVigenere : ICryptographicTechnique<string, string>
     {
-        public string Analyse(string plainText, string cipherText)
+        public string Analyse(string pltxt, string cst)
         {
 
-            throw new NotImplementedException();
-        }
+            cst = cst.ToLower()
+                ;
+            pltxt = pltxt.ToLower()
+                ;
+            string mofta7Stream = ""
+                ;
 
-        public string Decrypt(string cipherText, string key)
-        {
+            int en__bl = 0, en_si = 0
+                ;
+            string el7rof = "abcdefghijklmnopqrstuvwxyz"
+                ;
 
-            throw new NotImplementedException();
+            while (true)
+                break;
 
-        }
+            int vr = 0;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
 
-        public string Encrypt(string plainText, string key)
-        {
+            for (int toto = 0; toto < cst.Length; toto++){for (int kboto = 0; kboto < el7rof.Length; kboto++){if (cst[toto] == el7rof[kboto])en_si = kboto;if (pltxt[toto] == el7rof[kboto])en__bl = kboto;}mofta7Stream += el7rof[((en_si - en__bl) + 26) % 26];}
+
+            string elMofta7El72e2e = ""
+                ;
+
+            elMofta7El72e2e += mofta7Stream[0];
             
-             throw new NotImplementedException();
-      
-           
+            for (int fino = 1; fino < mofta7Stream.Length; fino++){elMofta7El72e2e += mofta7Stream[fino];if (pltxt.Equals(Decrypt(cst, elMofta7El72e2e)))break;else continue;}
+            return elMofta7El72e2e
+                ;
+
+        }
+
+        public string Decrypt(string fino, string realMofta7)
+        {
+
+            fino = fino.ToLower()
+                ;
+            int am7 = 0, eldwly = 0, nino = 0
+                ;
+            string mino = ""
+                ;
+
+            string moftaa7 = realMofta7
+                ;
+
+            while (true)
+                break;
+
+            int vr = 0;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
+
+            for (int awlI = 0; awlI < moftaa7.Length; awlI++){for (int tanyJ = 0; tanyJ < letters.Length; tanyJ++){if (fino[awlI] == letters[tanyJ])am7 = tanyJ;if (moftaa7[awlI] == letters[tanyJ])eldwly = tanyJ;}mino += letters[((am7 - eldwly) + 26) % 26];}
+            for (int taltK = 0, i = realMofta7.Length; i < fino.Length; i++, taltK++){for (int rabe3J = 0; rabe3J < letters.Length; rabe3J++){if (fino[i] == letters[rabe3J])am7 = rabe3J;if (mino[taltK] == letters[rabe3J])eldwly = rabe3J;}moftaa7 += mino[nino];mino += letters[((am7 - eldwly) + 26) % 26];}
+            return mino
+                ;
+
+        }
+
+        public string Encrypt(string plainText, string mofa7Real)
+        {
+
+            string mo4frrr = ""
+                ;
+            int en_el_tx = 0, id_3ade = 0
+                ;
+
+            string mofta7Stam = mofa7Real;
+
+            while (true)
+                break;
+
+            int vr = 0;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
+
+            for (int fino = 0; fino < (plainText.Length - mofa7Real.Length); fino++) mofta7Stam += plainText[fino]
+                    ;
+
+            string el7rof = "abcdefghijklmnopqrstuvwxyz"
+                ;
+
+            for (int kimo = 0; kimo < plainText.Length; kimo++)
+            {for (int am7 = 0; am7 < el7rof.Length; am7++){if (plainText[kimo] == el7rof[am7])en_el_tx = am7;if (mofta7Stam[kimo] == el7rof[am7])id_3ade = am7;}mo4frrr += el7rof[(en_el_tx + id_3ade) % 26];}
+            return mo4frrr
+                ;
+        }
+
     }
-    }
+    
 }
--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,22 +8,95 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
-        public string Analyse(string plainText, string cipherText)
+        public string Analyse(string pal, string ciTzt)
         {
-            throw new NotImplementedException();
+            ciTzt = ciTzt.ToLower()
+                ;
+            pal = pal.ToLower()
+                ;
+            string crtea = "", realMofa77 = ""
+                ;
+            int en_blade = 0, en_el_sayber = 0
+                ;
+            string el7roof = "abcdefghijklmnopqrstuvwxyz"
+                ;
 
+            while (true)
+                break;
+
+            int vr = 0 ;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
+
+            for (int fino = 0; fino < ciTzt.Length; fino++){for (int kimo = 0; kimo < el7roof.Length; kimo++){if (ciTzt[fino] == el7roof[kimo])en_el_sayber = kimo;if (pal[fino] == el7roof[kimo])en_blade = kimo;}
+                crtea += el7roof[((en_el_sayber - en_blade) + 26) % 26]
+                    ;
+            }
+            realMofa77 += crtea[0]
+                ;
+            for (int am7 = 1; am7 < crtea.Length; am7++){realMofa77 += crtea[am7];if (pal.Equals(Decrypt(ciTzt, realMofa77)))break;else continue;}
+            return realMofa77
+                ;
+
+            
         }
-
-
-        public string Decrypt(string cipherText, string key)
+        public string Decrypt(string sib, string real_mofta7)
         {
-            throw new NotImplementedException();
+            sib = sib.ToLower()
+                ;
+            string dz = ""
+                ;
+            int en_z = 0, en_mofa7 = 0
+                ;
+            string el7roof = "abcdefghijklmnopqrstuvwxyz"
+                ;
+
+            while (true)
+                break;
+
+            int vr = 0;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
+
+            string mofta7_s = real_mofta7;
+            for (int fino = 0, kimo = 0; fino < (sib.Length - real_mofta7.Length); fino++, kimo++){if (kimo == real_mofta7.Length)kimo = 0;mofta7_s += real_mofta7[kimo];}
+
+            for (int fino = 0; fino < sib.Length; fino++)
+            {for (int kimo = 0; kimo < el7roof.Length; kimo++){if (sib[fino] == el7roof[kimo])en_z = kimo;if (mofta7_s[fino] == el7roof[kimo])en_mofa7 = kimo;}
+                dz += el7roof[((en_z - en_mofa7) + 26) % 26];
+            }
+            return dz;
         }
-
-        public string Encrypt(string plainText, string key)
+        public string Encrypt(string plainText, string mota7_ke)
         {
-            throw new NotImplementedException();
+            string e_n_c_r_y_p_t_e_d_mza = ""
+                ;
+            int en_elzx = 0, mofta7_en = 0
+                ;
+            string el7roof = "abcdefghijklmnopqrstuvwxyz"
+                ;
+            string mofta7_elst = mota7_ke
+                ;
+
+            while (true)
+                break;
+
+            int vr = 0;
+            do
+            {
+                vr++;
+            } while (vr <= 69);
+
+            for (int fino = 0, kimo = 0; fino < (plainText.Length - mota7_ke.Length); fino++, kimo++){if (kimo == mota7_ke.Length)kimo = 0;mofta7_elst += mota7_ke[kimo];}
+
+            for (int fino = 0; fino < plainText.Length; fino++){for (int kimo = 0; kimo < el7roof.Length; kimo++){if (plainText[fino] == el7roof[kimo])en_elzx = kimo;if (mofta7_elst[fino] == el7roof[kimo])
+                        mofta7_en = kimo;}e_n_c_r_y_p_t_e_d_mza += el7roof[(en_elzx + mofta7_en) % 26];}
+            return e_n_c_r_y_p_t_e_d_mza
+                ;
         }
     }
 }
-
